80ec5c1f3d086bad1d86f07d57719be3aa32ffa3
sam quave <samquave@sams-MacBook-Air.local>
2017-09-11 20:21:38 -0400

    html, css, js linked

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var friendName = ['Deadshot', 'Lex Luthor', 'The Joker', 'Sinestro', 'Reverse Flash'];
+    var locationName = ['Gotham City Bank', 'Arkham Asylum', 'LexCorp', 'Wayne Manor', 'Fortress of Solitude', 'Hall of Justice', 'S.T.A.R. Labs', 'Atlantis', 'Hall of Doom', 'Batcave'];
+    var weaponName = ['shoe', 'pencil', 'coffee mug', 'bowling pin', 'rolled up newspaper', 'spray on deodorant', 'lego brick', 'hot sauce', 'cattle prod', 'dental floss', 'mop', 'ice cube tray', 'toenail clippers', 'shoelace', 'sock full of batteries', 'extension cord', 'pizza box', 'lightbulb', 'music box', 'shoelace'];
+
+})
